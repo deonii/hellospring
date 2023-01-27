ce19b8c10afd58f76a0eeef9a0f2b3dc1983f24b
deonii <fishingman99@naver.com>
2023-01-27 23:50:50 +0900

    last commit

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -2,10 +2,14 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -16,7 +20,7 @@ public class MemberService {
 //    = new MemoryMemberRepository();
 
     public Long join(Member member) {
-        validateDuplicateMember(member); // 중복 회원 검증
+        validateDuplicateMember(member);
         memberRepository.save(member);
         return member.getId();
     }
